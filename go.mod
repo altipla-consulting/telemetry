cda132cbc0b21ca7c825025effc520e743983ced
Ernesto Alejo <ernestokarim@gmail.com>
2025-02-24 14:17:18 +0100

    Emite los fallos críticos con suficiente tiempo para que se envíen a Sentry.

--- go.mod
+++ go.mod
@@ -6,11 +6,11 @@ require (
 	github.com/altipla-consulting/env v0.2.2
 	github.com/altipla-consulting/errors v1.4.0
 	github.com/altipla-consulting/sentry v0.6.0
+	github.com/getsentry/sentry-go v0.29.1
 	github.com/lmittmann/tint v1.0.4
 )
 
 require (
-	github.com/getsentry/sentry-go v0.29.1 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 )
--- logging/fatal.go
+++ logging/fatal.go
@@ -4,12 +4,17 @@ import (
 	"context"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/altipla-consulting/telemetry"
+	"github.com/getsentry/sentry-go"
 )
 
 func Fatal(msg string, err error) {
 	slog.Log(context.Background(), LevelCritical, msg, slog.String("err", err.Error()))
 	telemetry.ReportError(context.Background(), err)
+	if os.Getenv("SENTRY_DSN") != "" {
+		sentry.Flush(5 * time.Second)
+	}
 	os.Exit(1)
 }
